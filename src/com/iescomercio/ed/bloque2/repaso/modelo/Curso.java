4085e94e74d084f439a88861e95d71f5e57c41cc
d <you@example.com>
2022-03-03 23:08:07 +0100

    Documentada <Curso>

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
@@ -7,9 +7,11 @@ public class Curso {
 	
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Metodo que sirve para eliminar un alumno.
+	 * @param dni
+	 * @throws Exception
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
 		if(dni.length()==9) {//comprobar la longitud del dni
 			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
@@ -18,16 +20,19 @@ public class Curso {
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Metodo que sirve para aniadir un alumno.
+	 * @param p
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Metodo que sirve para saber si un alumno esta registrado.
+	 * @param dni
+	 * @return boolean 
+	 */
 	public Boolean estaRegistrado(String dni) {
 		int i =0;
 		Boolean encontrado=false;
@@ -40,16 +45,17 @@ public class Curso {
 		return encontrado;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Metodo que sirve para crear una lista de alumnos.
+	 */
 	public Curso() {
 		listaAlumnos= new ArrayList<Persona>();
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Metodo que sirve para contar la cantidad de alumnos.
+	 * @return Integer
+	 */
 	public Integer numeroAlumnos() {return listaAlumnos.size();}
 	
 	
